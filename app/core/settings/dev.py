0f3121068a8400d16a35adb93b356b7b4b8d766c
alihamza.khan05 <alihamza.khan05@gmail.com>
2025-12-23 08:36:31 +0000

    new

--- app/core/settings/dev.py
+++ app/core/settings/dev.py
@@ -1,7 +1,7 @@
 from app.core.settings.base import BaseConfig
 
 class DevConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI: str = "sqlite:///./test.db"
+    SQLALCHEMY_DATABASE_URI: str = "postgresql://user@localhost:5432/health-app-db"
     FIRST_SUPERUSER: str = "admin"
     FIRST_SUPERUSER_PASSWORD: str = "admin"
     BACKEND_CORS_ORIGINS: list[str] = ["*"]
